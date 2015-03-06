8e3dc1d0a58b5000f8fe066e426fde6693808b2e
Rohan Barman <Rohan1Barman@gmail.com>
2015-03-06 10:07:00 -0500

    Minimal play for favorites
    -Log in and start playing favorite songs.
    - Use arrow keys to go to previous or next song

---
+++ script.js
@@ -0,0 +1,85 @@
+var	spinner = new Spinner({
+	lines: 12, // The number of lines to draw
+	length: 7, // The length of each line
+	width: 5, // The line thickness
+	radius: 10, // The radius of the inner circle
+	color: '#000', // #rbg or #rrggbb
+	speed: 1, // Rounds per second
+	trail: 100, // Afterglow percentage
+	shadow: true // Whether to render a shadow
+});
+
+function sayHello() {
+	console.log("i'm saying hello right now");
+}
+
+var tracks = [];
+var currentIndex = 0;
+
+function getFavoriteSongs() {
+	console.log("getting favorite songs");
+	spinner.spin(document.getElementById("post-login-container")); 
+
+	var page_size = 200;
+	SC.get('/me/favorites',{limit:page_size,linked_partitioning: 1},function(data){
+		$(data.collection).each(function(i,track){
+			console.log(track.title + " " + track.artwork_url);
+			tracks.push(track.id);
+		});
+		console.log("done");
+		spinner.stop();
+		$("#icon").attr("src","./resources/orange_icon.png");
+		$('#icon').fadeIn(3000);
+
+		playSongs(currentIndex);
+	});
+}
+
+function playSongs(index) {
+	SC.stream("/tracks/" + tracks[index], 
+		{	onfinish: 
+			function() {
+				console.log('track finished');
+				playSongs(++currentIndex);
+			}
+
+		},
+
+		function(sound) {
+			sound.play();
+			document.onkeydown = function(e) {
+				switch (e.keyCode) {
+			        case 37: // left
+			        console.log("will play previous song");
+			        sound.stop();
+			        playSongs(--currentIndex);
+			        break;
+			        case 38: // up
+			        console.log("repost song");
+			        break;
+			        case 39: //right 
+			        console.log("will play next song");
+			        sound.stop();
+			        playSongs(++currentIndex);
+			        break;
+			        case 40: //diown
+			        console.log("favorite song");
+			        break;
+			    }
+			};
+		});
+}	
+
+function logIn() {
+	SC.initialize({
+		client_id: 'b713150b1588a27489dda2c15e8f95c5',
+		redirect_uri: 'http://127.0.0.1/~rohan/SCPlaylistDownloader/callback.html'
+	});
+
+	SC.connect(function() {
+		console.log("successfully connected");
+		$('#icon').fadeOut(1000);
+		$('#post-login-container').show();
+		getFavoriteSongs();
+	});
+}
