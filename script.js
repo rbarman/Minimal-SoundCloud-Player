60dd64af577348c3f7559badfa43cb3e83372b4a
Rohan Barman <Rohan1Barman@gmail.com>
2015-03-06 17:55:41 -0500

    tracks[] will hold id and title
    Having the track’s title stored will make debugging easier

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function sayHello() {
 	console.log("i'm saying hello right now");
 }
 
+var track = {
+	id : "",
+	title : ""
+};
+
 var tracks = [];
 var currentIndex = 0;
 
@@ -20,10 +25,10 @@ var currentIndex = 0;
 function getStream() {
 	console.log("getting my stream!");
 	spinner.spin(document.getElementById("post-login-container")); 
-	var page_size = 200;
+	var page_size = 5;
 	SC.get('/me/activities/tracks/affiliated',{limit:page_size,linked_partitioning:1},function(data){
 		$(data.collection).each(function(i,track){
-			tracks.push(track.origin.id);
+			tracks.push({id :track.origin.id, title : track.origin.title});
 		});
 		console.log("done");
 		spinner.stop();
@@ -39,41 +44,42 @@ function getFavoriteSongs() {
 
 	var page_size = 200;
 	SC.get('/me/favorites',{limit:page_size,linked_partitioning: 1},function(data){
+		console.log(data);
 		$(data.collection).each(function(i,track){
-			tracks.push(track.id);
+			// the track object returned from favorites is slightly diff than stream tracks
+			tracks.push({id :track.id, title : track.title});
 		});
 		console.log("done");
 		spinner.stop();
 		$("#icon").attr("src","./resources/orange_icon.png");
 		$('#icon').fadeIn(3000);
-
 		playSongs(currentIndex);
 	});
 }
 
 var paused = false;
 function playSongs(index) {
-	SC.stream("/tracks/" + tracks[index], 
+	SC.stream("/tracks/" + tracks[index].id, 
 		{	onfinish: 
 			function() {
-				console.log('track finished');
+				console.log(tracks[index].title + ' finished');
 				playSongs(++currentIndex);
 			}
-
 		},
 
 		function(sound) {
 			sound.play();
+			console.log("playing " + tracks[index].title) + " w/ id : " + tracks[index].id;
 			document.onkeydown = function(e) {
 				switch (e.keyCode) {
 					case 32: // space
 					if(paused == true) {
-						console.log("resume playing");
+						console.log("resuming " + tracks[index].title);
 						sound.play();
 						paused = false; 
 					}
 					else {
-						console.log("pausing");
+						console.log("pausing " + tracks[index].title);
 						sound.pause();
 						paused = true;
 					}
@@ -84,7 +90,7 @@ function playSongs(index) {
 			        playSongs(--currentIndex);
 			        break;
 			        case 38: // up
-			        console.log("repost song");
+			        console.log("repost " + tracks[index].title);
 			        break;
 			        case 39: //right 
 			        console.log("will play next song");
@@ -92,7 +98,7 @@ function playSongs(index) {
 			        playSongs(++currentIndex);
 			        break;
 			        case 40: //down
-			        console.log("favorite song");
+			        console.log("favorite " + tracks[index].title);
 			        break;
 			    }
 			};
