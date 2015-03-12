51b2b2e7056bd4157cff6ad8908c544a9e73375e
Rohan Barman <Rohan1Barman@gmail.com>
2015-03-11 22:34:26 -0400

    First steps towards search_box
    key pressing enter will either open up or close a search box. Pressing
    esc will close the search box.
    
    No actual searching has been done yet — just setting up

--- script.js
+++ script.js
@@ -71,6 +71,9 @@ function playSongs() {
 		});
 }	
 
+
+var isSearching = false;
+
 function playSongsCallback(sound){
 	sound.play();
 
@@ -82,95 +85,139 @@ function playSongsCallback(sound){
 
 	if(currentIndex != 0)
 		$("#icon").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+	document.onkeypress = function(e){
+		switch(e.keyCode){
+			case 13: // enter
+				// if we are not searching (normal playing), a search box will open up.
+				// if we are searching, the searchbox will go down and make the search. 
+				if(!isSearching) {
+					console.log("pressed enter - will show search_box");
+					$('#track-container').fadeOut(function(){
+						$(this).replaceWith( 
+							'<input type="text" id="search_box" autofocus ="autofocus">'
+						);
+						isSearching = true;
+					});
+				}
+				else if(isSearching){
+					console.log("pressed enter - will close search_box");
+					var search_entry = $('#search_box').val();
+					console.log("will search for : " + search_entry);
+
+					$('#search_box').fadeOut(function(){
+						$(this).replaceWith('<div id = "track-container"> </div>');
+						$('#track-container').text(tracks[currentIndex].title);
+						isSearching = false;
+					})
+				}
+				break;
+
+			case 27: //esc
+				// pressing escape should close #search_box if it is currently open
+				// also will set isSearching = false;
+				console.log("pressed esc");
+				$('#search_box').fadeOut(function(){
+					$(this).replaceWith('<div id = "track-container"> </div>');
+					$('#track-container').text(tracks[currentIndex].title);
+					isSearching = false;
+				})
+				break;
+		}
+	}
+
 	document.onkeydown = function(e) {
-		switch (e.keyCode) {
+
+		if(!isSearching){
+			switch (e.keyCode) {
 				case 32: // space
 				// pause song if it is playing, unpause if it is paused
-					if(paused == true) {
-						console.log("resuming " + tracks[currentIndex].title);
-						sound.play();
-						paused = false; 
-					}
-					else {
-						console.log("pausing " + tracks[currentIndex].title);
-						sound.pause();
-						paused = true;
-					}
-					break;
+				if(paused == true) {
+					console.log("resuming " + tracks[currentIndex].title);
+					sound.play();
+					paused = false; 
+				}
+				else {
+					console.log("pausing " + tracks[currentIndex].title);
+					sound.pause();
+					paused = true;
+				}
+				break;
 
 				case 37: // left
 					// play previous song
-			        console.log("will play previous song");
-			        sound.stop();
-			        currentIndex--;
-			        playSongs();
-			        break;
+					console.log("will play previous song");
+					sound.stop();
+					currentIndex--;
+					playSongs();
+					break;
 
 			    case 82: // r
 			    	// repost current song
-			        console.log("repost " + tracks[currentIndex].title);
-			        repostCurrentSong();
-			        break;
+			    	console.log("repost " + tracks[currentIndex].title);
+			    	repostCurrentSong();
+			    	break;
 
 		        case 39: //right 
 		        	// play the next song
-			        console.log("will play next song");
-			        sound.stop();
-			        currentIndex++;
-			        playSongs();
-			        break;
+		        	console.log("will play next song");
+		        	sound.stop();
+		        	currentIndex++;
+		        	playSongs();
+		        	break;
 
 				case 70: //f
 					// favorite current song
-			        console.log("favorite " + tracks[currentIndex].title);
-			        favoriteCurrentSong();
-			        break;
+					console.log("favorite " + tracks[currentIndex].title);
+					favoriteCurrentSong();
+					break;
 
 		        case 70: // f
 		        	// switch to playing favorites
-			        console.log("going to get favorites");
-			        tracks.length = 0;
-			        currentIndex = 0;
-			        sound.stop();
-			        getFavoriteSongs();
-			        break;
+		        	console.log("going to get favorites");
+		        	tracks.length = 0;
+		        	currentIndex = 0;
+		        	sound.stop();
+		        	getFavoriteSongs();
+		        	break;
 
 		        case 83: // s
 		        	// switch to playing on stream. 
-			        console.log("going to get stream");
-			        tracks.length = 0;
-			        currentIndex = 0;
-			        sound.stop();
-			        getStream();
-			        break;
-			    }
-			};
+		        	console.log("going to get stream");
+		        	tracks.length = 0;
+		        	currentIndex = 0;
+		        	sound.stop();
+		        	getStream();
+		        	break;
+		        }
+		    };
 		}
+	}
 
-function favoriteCurrentSong(){
-	SC.put('/me/favorites/' + tracks[currentIndex].id, function(data){
+	function favoriteCurrentSong(){
+		SC.put('/me/favorites/' + tracks[currentIndex].id, function(data){
 		// console.log(data);
 	});
-}
+	}
 
-function repostCurrentSong() {
-	SC.put('/e1/me/track_reposts/' + tracks[currentIndex].id, function(data){
+	function repostCurrentSong() {
+		SC.put('/e1/me/track_reposts/' + tracks[currentIndex].id, function(data){
 		// console.log(data);
 	});
-}
+	}
 
-function logIn() {
-	SC.initialize({
-		client_id: 'b713150b1588a27489dda2c15e8f95c5',
-		redirect_uri: 'http://127.0.0.1/~rohan/SCPlaylistDownloader/callback.html'
-	});
+	function logIn() {
+		SC.initialize({
+			client_id: 'b713150b1588a27489dda2c15e8f95c5',
+			redirect_uri: 'http://127.0.0.1/~rohan/SCPlaylistDownloader/callback.html'
+		});
 
-	SC.connect(function() {
-		console.log("successfully connected");
-		$('#icon').fadeOut(1000);
-		$('#post-login-container').show();
+		SC.connect(function() {
+			console.log("successfully connected");
+			$('#icon').fadeOut(1000);
 		// getFavoriteSongs();
 		getStream();
 		// sayHello();
 	});
-}
+	}
